9a2f2479f364b3c735b7d56830afafbb4ed63172
gabrielgsouzaDev <gabriel.g.souza2025@gmail.com>
2025-11-21 13:01:18 -0300

    Kernel.php e CorsMiddleware.php add

---
+++ app/Http/Middleware/CorsMiddleware.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class CorsMiddleware
+{
+    public function handle(Request $request, Closure $next)
+    {
+        // Responde imediatamente a requisições OPTIONS
+        if ($request->getMethod() === "OPTIONS") {
+            return response()->json([], 200)
+                ->header('Access-Control-Allow-Origin', '*')
+                ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+                ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With');
+        }
+
+        $response = $next($request);
+
+        return $response
+            ->header('Access-Control-Allow-Origin', '*')
+            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+            ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With');
+    }
+}
